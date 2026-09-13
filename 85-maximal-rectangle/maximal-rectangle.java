c51908dd7a806dcbe75c8760eeb1bb788e7c7d3a
Vicky kumar <krvicky709@gmail.com>
2026-09-13 12:50:39 +0530

    Time: 18 ms (77.22%) | Memory: 50.7 MB (52.94%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -0,0 +1,77 @@
+class Solution {
+
+    public int maximalRectangle(char[][] matrix) {
+
+        if (matrix.length == 0) {
+            return 0;
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[] heights = new int[cols];
+
+        int maxArea = 0;
+
+        for (int i = 0; i < rows; i++) {
+
+            // Step 1: Build histogram
+            for (int j = 0; j < cols; j++) {
+
+                if (matrix[i][j] == '1') {
+                    heights[j]++;
+                } else {
+                    heights[j] = 0;
+                }
+            }
+
+            // Step 2: Largest Rectangle in Histogram
+            int area = largestRectangle(heights);
+
+            maxArea = Math.max(maxArea, area);
+        }
+
+        return maxArea;
+    }
+
+
+    private int largestRectangle(int[] heights) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        int maxArea = 0;
+
+        for (int i = 0; i <= heights.length; i++) {
+
+            int currentHeight;
+
+            if (i == heights.length) {
+                currentHeight = 0;
+            } else {
+                currentHeight = heights[i];
+            }
+
+            while (!stack.isEmpty()
+                    && heights[stack.peek()] > currentHeight) {
+
+                int height = heights[stack.pop()];
+
+                int width;
+
+                if (stack.isEmpty()) {
+                    width = i;
+                } else {
+                    width = i - stack.peek() - 1;
+                }
+
+                int area = height * width;
+
+                maxArea = Math.max(maxArea, area);
+            }
+
+            stack.push(i);
+        }
+
+        return maxArea;
+    }
+}
